c15f62ba008ac23cfe48307ae14ab8951dd33a2d
Ryotaro Takeda <nmeeaiwxntwsoetnlelin@gmail.com>
2017-09-26 22:05:22 +0900

    Implements login

--- app/src/main/java/apps/ryotaro/myfinance/MainActivity.java
+++ app/src/main/java/apps/ryotaro/myfinance/MainActivity.java
@@ -4,56 +4,68 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // get firebase Instance.
-        mAuth = FirebaseAuth.getInstance();
-
-        // define authListener
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // if user is signed in.
-                } else {
-                    // User is signed out.
-                }
-
-            }
-        };
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
+        mAuth = FirebaseAuth.getInstance();
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            // already Login
+            ((TextView) findViewById(R.id.textView)).setText("ログインしてるよ");
+        } else {
+            // not login
+            ((TextView) findViewById(R.id.textView)).setText("ログインしてないよ");
+        }
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
+    public void onClick(View v) {
+        if (v != null) {
+            if (v.getId() == R.id.login_button) {
+                String email = ((EditText) findViewById(R.id.email)).getText().toString();
+                String password = ((EditText) findViewById(R.id.password)).getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    ((TextView) findViewById(R.id.textView)).setText("ログインしてるよ");
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    ((TextView) findViewById(R.id.textView)).setText("ログイン失敗");
+                                }
+                            }
+                        });
+            }
         }
     }
 }
