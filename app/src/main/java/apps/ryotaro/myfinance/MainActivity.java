165234fc0f9275e79fd78a1f34b836e1ceb9761f
Ryotaro Takeda <nmeeaiwxntwsoetnlelin@gmail.com>
2017-12-13 22:36:46 +0900

    Remove unnecessary lines

--- app/src/main/java/apps/ryotaro/myfinance/MainActivity.java
+++ app/src/main/java/apps/ryotaro/myfinance/MainActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
@@ -28,50 +28,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-        ((Button) findViewById(R.id.login_button)).setOnClickListener(this);
-        ((Button) findViewById(R.id.logout_button)).setOnClickListener(this);
-        ((Button) findViewById(R.id.spread_button)).setOnClickListener(this);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null) {
-            // already Login
-            ((TextView) findViewById(R.id.textView)).setText("ログインしてるよ");
-        } else {
-            // not login
-            ((TextView) findViewById(R.id.textView)).setText("ログインしてないよ");
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v != null) {
-            if (v.getId() == R.id.login_button) {
-                String email = ((EditText) findViewById(R.id.email)).getText().toString();
-                String password = ((EditText) findViewById(R.id.password)).getText().toString();
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    ((TextView) findViewById(R.id.textView)).setText("ログインしてるよ");
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    ((TextView) findViewById(R.id.textView)).setText("ログイン失敗");
-                                }
-                            }
-                        });
-            } else if (v.getId() == R.id.logout_button) {
-                mAuth.signOut();
-            } else if (v.getId() == R.id.spread_button) {
-                startActivity(new Intent(MainActivity.this, SpreadActivity.class));
-            }
-        }
     }
 }
