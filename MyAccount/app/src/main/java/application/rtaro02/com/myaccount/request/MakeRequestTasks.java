f9adb8322f15b8f70b345161dc1ef7fa88f905d6
Rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2018-04-22 18:08:08 +0900

    Implement insert row

--- MyAccount/app/src/main/java/application/rtaro02/com/myaccount/request/MakeRequestTasks.java
+++ MyAccount/app/src/main/java/application/rtaro02/com/myaccount/request/MakeRequestTasks.java
@@ -5,15 +5,24 @@ import android.os.AsyncTask;
 import android.widget.TextView;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.BatchUpdateSpreadsheetRequest;
+import com.google.api.services.sheets.v4.model.BatchUpdateSpreadsheetResponse;
+import com.google.api.services.sheets.v4.model.DimensionRange;
+import com.google.api.services.sheets.v4.model.InsertDimensionRequest;
+import com.google.api.services.sheets.v4.model.Request;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -87,9 +96,49 @@ public class MakeRequestTasks extends AsyncTask<Void, Void, Void> {
      * データを書き込むメソッド
      * @throws IOException
      */
-    private void getRangeFromApi() throws IOException {
+    private void getRangeFromApi() throws IOException, GeneralSecurityException {
         String spreadsheetId = "1sXe8CICyRq3mP6SVelPfUefLTnl0hrSuHbjEAMb1Phw";
-        System.out.println(this.mService.spreadsheets().values().batchGet(spreadsheetId));
+        List<Request> requests = new ArrayList<>();
+        DimensionRange dimensionRange = new DimensionRange();
+        dimensionRange.setSheetId(0);
+        dimensionRange.setDimension("ROWS");
+        dimensionRange.setStartIndex(0);
+        dimensionRange.setEndIndex(1);
+        InsertDimensionRequest insertDimension = new InsertDimensionRequest();
+        insertDimension.setRange(dimensionRange);
+        Request r = new Request();
+        r.setInsertDimension(insertDimension);
+        requests.add(r);
+        BatchUpdateSpreadsheetRequest requestBody = new BatchUpdateSpreadsheetRequest();
+        requestBody.setRequests(requests);
+
+        Sheets.Spreadsheets.BatchUpdate request =
+                this.mService.spreadsheets().batchUpdate(spreadsheetId, requestBody);
+
+        BatchUpdateSpreadsheetResponse response = request.execute();
+
+        // TODO: Change code below to process the `response` object:
+        System.out.println(response);
+
+        System.out.println(this.mService.spreadsheets().values().batchGet(spreadsheetId).execute());
+    }
+
+    public static Sheets createSheetsService() throws IOException, GeneralSecurityException {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+        // TODO: Change placeholder below to generate authentication credentials. See
+        // https://developers.google.com/sheets/quickstart/java#step_3_set_up_the_sample
+        //
+        // Authorize using one of the following scopes:
+        //   "https://www.googleapis.com/auth/drive"
+        //   "https://www.googleapis.com/auth/drive.file"
+        //   "https://www.googleapis.com/auth/spreadsheets"
+        GoogleCredential credential = null;
+
+        return new Sheets.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("Google-SheetsSample/0.1")
+                .build();
     }
 
     @Override
