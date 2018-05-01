ebc0ea9897475ad43a74a06fd8e5ca140d853197
Rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2018-05-01 13:33:19 +0900

    Remove unnecessary sentences

--- MyAccount/app/src/main/java/application/rtaro02/com/myaccount/request/MakeRequestTasks.java
+++ MyAccount/app/src/main/java/application/rtaro02/com/myaccount/request/MakeRequestTasks.java
@@ -149,24 +149,6 @@ public class MakeRequestTasks extends AsyncTask<Void, Void, Void> {
         System.out.println(this.mService.spreadsheets().values().batchGet(spreadsheetId).execute());
     }
 
-//    public static Sheets createSheetsService() throws IOException, GeneralSecurityException {
-//        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-//        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-//
-//        // TODO: Change placeholder below to generate authentication credentials. See
-//        // https://developers.google.com/sheets/quickstart/java#step_3_set_up_the_sample
-//        //
-//        // Authorize using one of the following scopes:
-//        //   "https://www.googleapis.com/auth/drive"
-//        //   "https://www.googleapis.com/auth/drive.file"
-//        //   "https://www.googleapis.com/auth/spreadsheets"
-//        GoogleCredential credential = null;
-//
-//        return new Sheets.Builder(httpTransport, jsonFactory, credential)
-//                .setApplicationName("Google-SheetsSample/0.1")
-//                .build();
-//    }
-
     @Override
     protected void onPreExecute() {
         //mOutputText.setText("");
